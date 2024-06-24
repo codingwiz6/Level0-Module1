87ad595edfcab1d4b93d6201d8f2e0c8b3e19c5d
codingwiz6 <64503808+codingwiz6@users.noreply.github.com>
2024-06-24 18:55:28 +0000

    June 24

--- src/_05_for_loops/_1_my_ages/MyAges.java
+++ src/_05_for_loops/_1_my_ages/MyAges.java
@@ -8,19 +8,21 @@ public class MyAges {
 
 		// Make an int variable called age, to track how many times the loop has repeated
 		//        Start its value at zero.
-		
+		int age = 0;
 		// Change the value of yourCurrentAge to match your age
-		int yourCurrentAge = 0;
+		int yourCurrentAge = 13;
 				
 		
 		//  WHILE LOOP. Start a while loop to repeat while age <= yourCurrentAge
+				while (age<= yourCurrentAge){
+
 				
 			//  Print the age variable
-		
+		System.out.println(age);
 			//  Add one to the age variable
-		
+		age++;
 		//  End the while loop here
-
+				}
 		//  Run the program. Did it print all your ages from 0 to your current age?
 		
 		/*** Now you are going to do the same thing, but this time use a for loop. 
@@ -30,11 +32,12 @@ public class MyAges {
 		
 		//  FOR LOOP. Start a for loop to repeat while age <= your age, like this:
 		//  for ( age=0; age <= yourAge; age +=1 ) {
-		
+		for ( age=0;age<=yourCurrentAge;age+=1){
+	
 			//  Print the age variable
-		
+		System.out.println(age);
 		//  End the for loop here
-		
+		}
 		//  Run the program again. 		
 		//  Check that both loops printed the same numbers to the console
 	}
---
+++ src/_05_for_loops/_2_badgers/badger.java
@@ -0,0 +1,17 @@
+package _05_for_loops._2_badgers;
+
+public class badger {
+    public static void main(String[] args) {
+        for(int i = 0;i<2;i++){
+            for(int j=0;j<12;j++){
+                System.out.print("Badger,");
+            }
+            System.out.println("");
+            for(int j=0;j<2;j++){
+                System.out.print("Mushroom,");
+            }
+            System.out.println("");
+        }
+        System.out.println("A snake!!!");
+    }
+}
